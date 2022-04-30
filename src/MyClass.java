ffeb612f2b2edba84477bba70282378ff81826a7
steve <steve128@yandex.ru>
2022-04-30 15:19:10 +0300

    initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            count++;
+            System.out.println(count);
+        }
+    }
+}
